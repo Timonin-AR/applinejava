45661d76d63e29b6449c014be907ae11435ecd74
Timonin-AR <wow.creepok.ru@mail.ru>
2020-04-15 15:50:45 +0300

    Перевод из двоичной в десятичную систему счисления

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class test {
-    public static void main (String[] args){
-        System.out.println ("Здарова епта");
+    public static void main (String args[]){
+        System.out.println ("Введите число в бинарном формате");
+        Scanner scan = new Scanner (System.in);//Считывание данных с консоли
+        String x = scan.nextLine();
+        System.out.println ("Число в двоимном формате " + Integer.parseInt(x,2));
+
+
     }
 }
